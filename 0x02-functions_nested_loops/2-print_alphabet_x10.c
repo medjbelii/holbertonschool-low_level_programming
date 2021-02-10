c0344d74de46151487fb051462c4139d0181e0b5
medjbelii <med.jbely@gmail.com>
2021-02-10 15:25:56 +0100

    printalphabets10X

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include 'holberton.h'
+/**
+ *
+ * print 10 times the alphabet
+ * 
+ **/
+void print_alphabet_x10(void);
+{
+    int i = 0;
+    char alpha;
+
+    while ( i < 10)
+{
+    alpha = a
+    while (a <= 'z')
+{
+    _putchar(alpha)
+    alpha++
+}
+    _putchar('/n')
+    i++
+}
+}
