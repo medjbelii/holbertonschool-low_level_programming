2e81a75badcbdae777eaae361e793215c8d4dc7a
medjbelii <med.jbely@gmail.com>
2021-02-11 14:22:12 +0100

    lessgoo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_to_98 - print from n to 98
+ * print_to_98 - prints from n to 98
  * @n: int
  * Return: 0
  */
@@ -9,33 +9,28 @@ void print_to_98(int n)
 {
 int a;
 
-    if (n < 98)
-    {
-            a= n;
-        while (a =< 98)
-        {   
-            if (a != 98)
-                printf("%i, ", a);
-                else
-                printf("%i\n", a);
-                a++
-        }
-    }            
-    else (n > 98)
-    {
-            a= n;
-        while (a >= 98)
-        {
-            if (a != 98) 
-                printf("%i, ", a);
-                else
-                printf("%i\n", a);
-                a-- 
-        }
-    }            
-}
-
-
-        
-
+	if (n < 98)
+	{
+		a = n;
+		while (a <= 98)
+		{
+			if (a != 98)
+			printf("%i, ", a);
+			else
+			printf("%i\n", a);
+			a++;
+		}
+	}
+	else
+	{
+		a = n;
+		while (a >= 98)
+		{
+			if (a != 98)
+			printf("%i, ", a);
+			else
+			printf("%i\n", a);
+			a--;
+		}
+	}
 }
