c83433e2a6b5fccdfbb3a2426dec07ef1c4c18b6
medjbelii <med.jbely@gmail.com>
2021-02-11 11:02:58 +0100

    hallas

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * add two integers and retrun the result
+ * @a: variable a
+ * @b: variable b
+ * Return: c
+ **/
+
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
