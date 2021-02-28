6315cdc7e4ee779dade3338c7937c98519feb522
medjbelii <med.jbely@gmail.com>
2021-03-01 00:28:01 +0100

    moooore

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reverse_array - concats two strings
+ * @a: destination
+ * @n: source
+ * Return: el jaw el kol
+ */
+void reverse_array(int *a, int n)
+{
+int x;
+int y;
+int z;
+if (n % 2 != 0)
+z = n + 1;
+else
+z = n;
+for (x = 0; x < z / 2; x++)
+{
+y = a[x];
+a[x] = a[n - 1 - x];
+a[n - 1 - x] = y;
+}
+}
