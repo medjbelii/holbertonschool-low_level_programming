43309120fa932794908ad28b20c2575f7d76e74d
medjbelii <med.jbely@gmail.com>
2021-03-22 18:28:11 +0100

    tnaket

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - print
+* Return: 0
+**/
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+        if ((a != 113)  && (a != 101))
+            putchar(a);
+        putchar('\n');
+}
