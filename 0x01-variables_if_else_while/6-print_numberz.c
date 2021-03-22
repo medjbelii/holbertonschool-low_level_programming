7f50d5f70d19f32f7988b055d02f073d5fa67c2d
medjbelii <med.jbely@gmail.com>
2021-03-22 19:01:11 +0100

    haala

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
-    #include <stdio.h>
-    /**
-    * main - print numberz
-    * Return: 0
-    **/
-    int main(void)
-    {
-        int a;
+#include <stdio.h>
+/**
+* main - print numberz
+* Return: 0
+**/
+int main(void)
+{
+	int a;
 
-        for (a = '0'; a <= '9'; a++)
-            putchar(a);
-        putchar('\n');
-        return (0);
-    }
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
