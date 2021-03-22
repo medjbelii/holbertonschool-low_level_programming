90a47858c6de1e399dbb83a471766b582c88caba
medjbelii <med.jbely@gmail.com>
2021-03-22 21:34:27 +0100

    pronto

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - last digit
+* Return: 0
+**/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (b = 'a'; b < 'g'; b++)    
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
