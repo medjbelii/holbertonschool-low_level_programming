5c2754af3b2c6c706c6bd59dda7994249dabf239
medjbelii <med.jbely@gmail.com>
2021-03-22 17:25:59 +0100

    asal

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - last digit
+* @A -char
+* @a -char
+* Return: 0
+**/
+int main(void)
+{
+	char a = 'a';
+	char A = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
