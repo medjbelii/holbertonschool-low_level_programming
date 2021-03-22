51e7c99555fba90ae8d8c32c7bf9edf7a98ea306
medjbelii <med.jbely@gmail.com>
2021-03-22 18:31:39 +0100

    soft

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,8 @@ int main(void)
 	int a;
 
 	for (a = 97; a < 123; a++)
-        if ((a != 113)  && (a != 101))
-            putchar(a);
-        putchar('\n');
-        return (0);
+		if ((a != 113)  && (a != 101))
+			putchar(a);
+		putchar('\n');
+		return (0);
 }
